94ec753168d49c90b0acb4fabb7e67d17928df9b
samucnz21 <samuel_mutxabuya@hotmail.com>
2019-01-31 20:50:21 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var sharedsession = require("express-socket.io-session");
 var server = require('http').Server(app);
-var io = require('socket.io')(server,{origins:'*:*'});
+var io = require('socket.io')(server);
 var session = require("express-session")({
   secret:"1234",
   resave:true,
@@ -27,6 +27,34 @@ app.use(session);
 io.use(sharedsession(session,{
   autoSave:true
 }));
+
+io.set('origins', '*:*');
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
 
 io.origins('*:*');
 
