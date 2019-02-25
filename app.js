cec8bd804dbeeb930ec4cc380878042066d8e575
samucnz21 <samuel_mutxabuya@hotmail.com>
2019-02-25 23:11:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -214,6 +214,6 @@ io.on('connection', function(socket)
 
 });
 
-server.listen(443,'0.0.0.0', function(){
-  console.log('listening on *:8080');
+server.listen(443, function(){
+  console.log('listening on *:443');
 });
